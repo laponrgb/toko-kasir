7c26e337d9cf6b9d5ab453799cde1c56ff31e14d
lapon <pranajatin@gmail.com>
2025-08-12 09:05:36 +0700

    fix input nomor telp

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -61,7 +61,7 @@ class PelangganController extends Controller
         $request->validate([
             'nama' => ['required', 'max:100'],
             'alamat' => ['nullable', 'max:580'],
-            'nomor_tlp' => ['nullable', 'max:13']
+            'nomor_tlp' => ['nullable', 'min_digits:12','max_digits:13']
         ]);
 
         $pelanggan->update($request->all());
